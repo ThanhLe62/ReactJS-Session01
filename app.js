823108b90573e63b9857dfde8e83887f0bb0454e
ThanhLe62 <137408389+ThanhLe62@users.noreply.github.com>
2023-08-26 19:42:34 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// Bài tập 1
+// const name1 = ["hello", "heyy", "hi"];
+// let name2 = name1.map(name1 => name1.toUpperCase());
+// console.log(name2);
+
+// Bài tập 3
+// const arr = [50, 30, 45, 20, 89, 60];
+// result = arr.filter((item) => {
+//     return item % 2 != 0;
+// });
+// console.log(result);
+
+// Bài tập 5
+// const arr1 = ["Welcome To Rikkei Academy", "Going to homework", "To Sleep"];
+// console.log(arr1.find(arr2 => arr2.length <= 8));
